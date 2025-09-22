27baee0682b8d6c720b61bf9dcf58f5edfb9db0d
sikai zhang <matthew.szhang91@gmail.com>
2025-09-22 18:22:40 +0800

    add to path

--- scripts/rtd_pre_install.sh
+++ scripts/rtd_pre_install.sh
@@ -7,9 +7,9 @@ set -euox pipefail
 java -version
 dot -V
 
-# This folder is on PATH and does not require sudo
 # Download latest plantuml.jar from github
 mkdir -p "${HOME}/.local/bin"
+export PATH="${HOME}/.local/bin:$PATH" # Add it to PATH
 curl -o "${HOME}/.local/bin/plantuml.jar" -L https://github.com/plantuml/plantuml/releases/latest/download/plantuml.jar
 # Create an executable script for plantuml
 printf '#!/bin/bash\nexec java -Djava.awt.headless=true -jar "${HOME}/.local/bin/plantuml.jar" "$@"\n' > "${HOME}/.local/bin/plantuml"
